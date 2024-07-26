deb563a7b662ea6594b1a675c6af1f3ae22828e9
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-07-25 20:33:02 -0400

    0.0.83

---
+++ 📥.js
@@ -0,0 +1,9 @@
+import { MountObserver, seed, BeHive } from 'be-hive/be-hive.js';
+import { emc as baseEMC } from './behivior.js';
+export const emc = {
+    ...baseEMC,
+    base: '📥',
+    enhPropKey: '📥',
+};
+const mose = seed(emc);
+MountObserver.synthesize(document, BeHive, mose);
---
+++ 📥.ts
@@ -0,0 +1,12 @@
+import { EMC, MountObserver, seed, BeHive } from 'be-hive/be-hive.js';
+import { emc as baseEMC} from './behivior.js';
+
+export const emc: EMC = {
+    ...baseEMC,
+    base: '📥',
+    enhPropKey: '📥',
+};
+
+const mose = seed(emc);
+
+MountObserver.synthesize(document, BeHive, mose);
