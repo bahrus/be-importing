7acbdd8280d9cd7be8e8876c9ef22ba5edeb0f02
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-07-26 17:09:41 -0400

    0.0.84

--- makeXtalElement.mjs
+++ makeXtalElement.mjs
@@ -1,43 +1,43 @@
 export class HTMLElement {
 }
 export function makeXtalElement(xtalElementProps, scripts, className) {
-    const { superclass, propDefaults, xform, lcXform, aka, actions, beFormAssociated, inferProps, propInferenceCriteria, propInfo, shadowRootMode, targetScope } = xtalElementProps;
+    const { inherits, propDefaults, xform, lcXform, aka, actions, formAss, inferProps, propInferenceCriteria, propInfo, shadowRootMode, targetScope } = xtalElementProps;
     if (scripts) {
         console.log(String.raw `<template onload=blow-dry-to-head><script type=module>
 ${scripts.map(x => x.toString() + '\n\r').join('\n\r')};
-${className && superclass ? `customElements.define('${superclass}', ${className})` : ''}
+${className && inherits ? `customElements.define('${inherits}', ${className})` : ''}
 </script></template>
 `);
     }
     console.log('<xtal-element');
-    if (superclass)
-        console.log(`superclass=${superclass}`);
+    if (inherits)
+        console.log(` inherits=${inherits} `);
     if (propDefaults) {
-        console.log(`prop-defaults='${JSON.stringify(propDefaults, undefined, 3)}'`);
+        console.log(` prop-defaults='${JSON.stringify(propDefaults, undefined, 3)}'`);
     }
     if (propInfo) {
-        console.log(`prop-info='${JSON.stringify(propInfo, undefined, 3)}'`);
+        console.log(` prop-info='${JSON.stringify(propInfo, undefined, 3)}' `);
     }
     if (xform) {
-        console.log(`xform='${JSON.stringify(xform, undefined, 3)}'`);
+        console.log(` xform='${JSON.stringify(xform, undefined, 3)}' `);
     }
     if (lcXform) {
-        console.log(`lc-xform='${JSON.stringify(lcXform, undefined, 3)}'`);
+        console.log(` lc-xform='${JSON.stringify(lcXform, undefined, 3)}' `);
     }
     if (actions) {
-        console.log(`actions='${JSON.stringify(actions, undefined, 3)}'`);
+        console.log(` actions='${JSON.stringify(actions, undefined, 3)}' `);
     }
-    if (beFormAssociated) {
-        console.log(`be-form-associated`);
+    if (formAss) {
+        console.log(` form-associated `);
     }
     if (inferProps) {
-        console.log(`infer-props`);
+        console.log(` infer-props `);
     }
     if (shadowRootMode) {
-        console.log(`shadow-root-mode=${shadowRootMode}`);
+        console.log(` shadow-root-mode=${shadowRootMode} `);
     }
     if (aka) {
-        console.log(`aka=${aka}`);
+        console.log(` aka=${aka} `);
     }
     console.log('></xtal-element>');
 }
--- makeXtalElement.mts
+++ makeXtalElement.mts
@@ -30,33 +30,33 @@ ${className && inherits ? `customElements.define('${inherits}', ${className})` :
 
     console.log('<xtal-element');
 
-    if(inherits) console.log(`inherits=${inherits} `);
+    if(inherits) console.log(` inherits=${inherits} `);
     if(propDefaults){
-        console.log(`prop-defaults='${JSON.stringify(propDefaults, undefined, 3)}' `);
+        console.log(` prop-defaults='${JSON.stringify(propDefaults, undefined, 3)}'`);
     }
     if(propInfo){
-        console.log(`prop-info='${JSON.stringify(propInfo, undefined, 3)}' `);
+        console.log(` prop-info='${JSON.stringify(propInfo, undefined, 3)}' `);
     }
     if(xform){
-        console.log(`xform='${JSON.stringify(xform, undefined, 3)}' `);
+        console.log(` xform='${JSON.stringify(xform, undefined, 3)}' `);
     }
     if(lcXform){
-        console.log(`lc-xform='${JSON.stringify(lcXform, undefined, 3)}' `);
+        console.log(` lc-xform='${JSON.stringify(lcXform, undefined, 3)}' `);
     }
     if(actions){
-        console.log(`actions='${JSON.stringify(actions, undefined, 3)}' `);
+        console.log(` actions='${JSON.stringify(actions, undefined, 3)}' `);
     }
     if(formAss){
-        console.log(`form-associated `);
+        console.log(` form-associated `);
     }
     if(inferProps){
-        console.log(`infer-props `);
+        console.log(` infer-props `);
     }
     if(shadowRootMode){
-        console.log(`shadow-root-mode=${shadowRootMode} `);
+        console.log(` shadow-root-mode=${shadowRootMode} `);
     }
     if(aka){
-        console.log(`aka=${aka} `);
+        console.log(` aka=${aka} `);
     }
     console.log('></xtal-element>');
 }
