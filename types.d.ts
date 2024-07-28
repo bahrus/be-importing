bd5d203508c2f93f89e8f85732121205da6ce419
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-07-28 17:46:25 -0400

    0.0.93

--- types.d.ts
+++ types.d.ts
@@ -1,13 +1,8 @@
-export {Localizer} from "./node_modules/trans-render/lib/mixins/types";
-export {XForm} from "./node_modules/trans-render/types";
-export {Actions as A, PropInfo} from './node_modules/trans-render/froop/types';
-
 import {
     EndUserProps as BeWrittenEndUserProps,
     AllProps as BeWrittenAllProps,
     Actions as BeWrittenActions,
 } from './node_modules/be-written/types';
-import { ActionOnEventConfigs } from "./node_modules/trans-render/froop/types";
 
 export interface EndUserProps extends BeWrittenEndUserProps{
     baseCDN?: string;
@@ -21,7 +16,6 @@ export type PAP = Partial<AP>;
 
 export type ProPAP = Promise<PAP>;
 
-export type POA = [PAP | undefined, ActionOnEventConfigs<PAP, Actions>];
 
 export interface Actions extends BeWrittenActions{
     //onPath(self: this): void;
